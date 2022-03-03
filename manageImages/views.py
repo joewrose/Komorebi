4cc560469099e4fde796e2469a80e954e863f7cf
Joe Rose <joewallacerose@hotmail.com>
2022-03-03 21:04:43 +0000

    Created templates directory and imageCloseup.html

--- manageImages/views.py
+++ manageImages/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the manage images page")
+    return render(request, 'manageImages/imageCloseup.html')
