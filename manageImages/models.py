dfa97cdca54b5ebcecbe11cea2cc163ea2d055ba
Joe Rose <joewallacerose@hotmail.com>
2022-03-03 21:24:44 +0000

    Created image model

--- manageImages/models.py
+++ manageImages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Picture(models.Model):
+    ID = models.IntegerField
+    image = models.ImageField()
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    time = models.DateTimeField()
+    # TODO - add 'Uploaded by' foreign key
+
+    def __str__(self):
+        return "ID: " + self.ID + " name: " + self.name
