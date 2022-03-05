4038e640b56081a4c73a417d00b98f6a65391479
Joe Rose <joewallacerose@hotmail.com>
2022-03-05 17:34:58 +0000

    Added adduser functionality (courtesy of ordinarycoders.com)

--- manageImages/views.py
+++ manageImages/views.py
@@ -1,12 +1,27 @@
 import datetime
 
+from django.contrib import messages
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.http import HttpResponse
 
-from manageImages.forms import ImageForm
+from manageImages.forms import ImageForm, NewUserForm
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("/closeup/")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request, "addUser.html", context={"register_form": form})
 
 
 def index(request):
