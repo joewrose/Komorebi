458b2152bd3f6eb606514b24214bce6a40c316c0
YIFAN WAN <2582286w@srudent.gla.ac.uk>
2022-03-09 20:19:26 +0000

    modify follow in models for it causes e304 error TODO - Get media files working

--- manageImages/models.py
+++ manageImages/models.py
@@ -6,6 +6,7 @@ from django.db import models
 # Create your models here.
 from komorebi.settings import MEDIA_URL
 
+
 class Picture(models.Model):
     ID = models.UUIDField(primary_key=True, default=uuid.uuid4())
     image = models.ImageField(upload_to='userImages/', null=False, blank=False)
@@ -14,6 +15,7 @@ class Picture(models.Model):
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     time = models.DateTimeField(auto_now_add=True)
+
     # TODO - add 'Uploaded by' foreign key
 
     def __str__(self):
@@ -25,18 +27,18 @@ class Picture(models.Model):
 class Vote(models.Model):
     user_ID = models.ForeignKey(User, on_delete=models.CASCADE)
     picture_ID = models.ForeignKey(Picture, on_delete=models.CASCADE)
-    ID = models.UniqueConstraint(fields=['user_ID','picture_ID'], name='Composite_Key')
+    ID = models.UniqueConstraint(fields=['user_ID', 'picture_ID'], name='Composite_Key')
     type = models.CharField(max_length=1, default='')
 
     def __str__(self):
-        return "User with ID " + str(self.user_ID) + " voted " + self.vote_type + " for photo with ID " + str(self.picture_ID)
+        return "User with ID " + str(self.user_ID) + " voted " + self.vote_type + " for photo with ID " + str(
+            self.picture_ID)
+
 
 class Follow(models.Model):
     follower_ID = models.ForeignKey(User, on_delete=models.CASCADE)
-    followed_ID = models.ForeignKey(User, on_delete=models.CASCADE)
+
     ID = models.UniqueConstraint(fields=['follower_ID', 'followed_ID'], name='Composite_Key')
 
     def __str__(self):
         return "User with ID " + str(self.follower_ID) + " followed user with ID" + str(self.followed_ID)
-
-
