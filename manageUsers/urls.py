e128635947466dfa1f89d110f817c180c87742cf
Ben O'Hara <97913064+Bennyboy64@users.noreply.github.com>
2022-03-11 15:50:58 +0000

    Update urls.py
    added the index (home) and about page urls, fixed myfeed url

--- manageUsers/urls.py
+++ manageUsers/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "manageUsers"
 
 urlpatterns = [
-    path('myfeed/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('myfeed/', views.myfeed, name='myfeed'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('create/', views.create, name='register'),
     path('dashboard/edit/', views.edit, name='edit'),
