36c29ba3e08941a20f5037ce7f7eef4e0bf6ca6a
Ben O'Hara <97913064+Bennyboy64@users.noreply.github.com>
2022-03-21 01:35:33 +0000

    Update urls.py
    added login path, updated the home path

--- manageUsers/urls.py
+++ manageUsers/urls.py
@@ -5,12 +5,13 @@ from django.contrib.auth.views import LogoutView
 app_name = "manageUsers"
 
 urlpatterns = [
-    path('home/', views.index, name='home'),
+    path('home/', views.home, name='home'),
     path('myfeed/', views.myfeed, name='myfeed'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', views.login, name='login'),
     path('create/', views.create, name='create'),
     path('dashboard/edit/', views.edit, name='edit'),
     path('profile/<ID>/', views.profile, name='profile'),
+    path('login', views.login, name='login'),
 ]
