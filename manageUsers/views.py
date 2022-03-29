97a30834fb7fb488db6cff0513d2f3412fbd1e26
Ben O'Hara <97913064+Bennyboy64@users.noreply.github.com>
2022-03-29 14:48:16 +0100

    Update views.py
    updated myfeed view

--- manageUsers/views.py
+++ manageUsers/views.py
@@ -27,17 +27,57 @@ def index(request):
     return HttpResponse("Welcome to the manageUsers Index page!")
 
 
-def myfeed(request):
+def myfeed(request): 
     if not request.user.is_authenticated:
-        redirect('/home/')
+        redirect('/home')
+        
+    else:
+        current_user = request.user
+        current_user_liked = Likes.objects.values('picture_ID').filter(user_ID = current_user)
+        current_user_disliked = Dislikes.objects.values('picture_ID').filter(user_ID = current_user)
+        
+        # if the current user has liked less than 5 pictures, return None value
+        if len(current_user_liked) < 5:
+            context_dict = {'myfeed_pictures': None}
 
-    context_dict = {}
+        else:
+            # create list for the recommended pictures
+            to_recommend = list()
+            # get list of users
+            users = CustomUser.objects.all()
+            users_checked = 0
 
-    pictures = Picture.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:9]
+            for us in users:
+                # stop after pictures from 30 users has been added
+                if users_checked <= 30:
+                    if us != current_user:
+                        user_likes = Likes.object.values('picture_ID').filter(user_ID = us)
+                        if len(user_likes) > 5:
+                            similar = 0
+                            # get the amount of pictures that have been liked by both
+                            for pic in user_likes:
+                                if pic is in current_user_liked:
+                                    similar += 1
+                            # if the amount of pictures liked by both is a least a third of the current user's liked pictures
+                            # add to 'to_recommend' list as long as the picture is not: 
+                            # already in 'to_recommend', already been liked by the current user, already been disliked by the current user
+                            if similar > (len(current_user_liked)/3):
+                                for pict in current_user_liked:
+                                    if pict not in to_recommend && pict not in current_user_liked && pict not in current_user_disliked:
+                                        to_recommend.append(pict)
+                                        users_checked += 1
+                    else:
+                        continue
+                else:
+                    break
 
-    context_dict["pictures"] = pictures
-    context_dict["title"] = "My Feed"
-    return render(request, "myfeed.html", context_dict)
+            # check if list contains pictures
+            if to_recommend == []:
+                context_dict = {'myfeed_pictures': "empty"}
+            else:
+                context_dict = {'myfeed_pictures': to_recommend}
+
+        return render(request, 'myfeed.html', context=context_dict)
 
 
 def dashboard(request):
